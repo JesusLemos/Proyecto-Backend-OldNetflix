34013c6dbff8767603cc68b99850e7fcfe60b821
Jesus Lemos <you@example.com>
2019-11-25 16:29:24 +0100

    Ultimos cambios

--- controladores/controladorRegistro.js
+++ controladores/controladorRegistro.js
@@ -2,45 +2,45 @@ const fs = require('fs');
 const { generateId } = require('../utils')
 
 
-const controladorRegistro = (req,res) => {
+const controladorRegistro = (req, res) => {
     const user = req.body;
     const dbUser = JSON.parse(fs.readFileSync('./BaseDeDatos/usuarios.json', 'utf-8'))
-    
+
     const logintudcontrasenya = /.{8,12}/
     const nombreUsuario = dbUser.usuarios.some(usuario => usuario.usuario === user.usuario)
-    if(!logintudcontrasenya.test(user.contrasenya)){
+    if (!logintudcontrasenya.test(user.contrasenya)) {
         return res
             .status(400)
-            .json({mensaje: "La contrasenya tiene que tener entre 8 a 12 caracteres"})
+            .json({ mensaje: "La contrasenya tiene que tener entre 8 a 12 caracteres" })
     }
-    if(nombreUsuario){
+    if (nombreUsuario) {
         return res
-        .status(400)
-        .json({mensaje:"El nombre de usuario ya existe"})
-        
+            .status(400)
+            .json({ mensaje: "El nombre de usuario ya existe" })
+
     }
 
 
 
     const ExisteUsuario = dbUser.usuarios.some(
         existe => existe.correo === user.correo);
-        if(ExisteUsuario){
-            return res
+    if (ExisteUsuario) {
+        return res
             .status(400)
-            .json({ mensaje: `email ya existe`})
-            }   
-            user.idusuario = dbUser.usuarios.length + 1;
-            user.token = generateId();
-            user.login = "False";
-            dbUser.usuarios.push(user)
+            .json({ mensaje: `email ya existe` });
+    };
+    user.idusuario = dbUser.usuarios.length + 1;
+    user.token = generateId();
+    user.login = "False";
+    dbUser.usuarios.push(user);
 
-            fs.writeFileSync('./BaseDeDatos/usuarios.json', JSON.stringify(dbUser, null, 2));
+    fs.writeFileSync('./BaseDeDatos/usuarios.json', JSON.stringify(dbUser, null, 2));
 
-            res.status(200).json(user)
+    res.status(200).json(user);
 
-        };
+};
 
-        module.exports = controladorRegistro
-    
+module.exports = controladorRegistro;
+
 
 
